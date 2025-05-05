138b1f939205252893e4485d579846966f5ba8e9
root <root@DESKTOP-SIDH0D9.>
2025-05-05 07:46:14 +0300

    Pagination for records

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -47,11 +47,14 @@ def handle_bookmarks():
     
     else:
         
-        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+        
+        bookmarks = Bookmark.query.filter_by(user_id=current_user).paginate(page=page, per_page=per_page)
         
         data = []
         
-        for bookmark in bookmarks:
+        for bookmark in bookmarks.items:
             data.append({
                 'id': bookmark.id,
                 'url': bookmark.url,
@@ -61,5 +64,15 @@ def handle_bookmarks():
                 'created_at': bookmark.created_at,
                 'updated_at': bookmark.updated_at,
             })
+        meta = {
+            "page": bookmarks.page,
+            "pages": bookmarks.pages,
+            "total_count": bookmarks.total,
+            "prev_page": bookmarks.prev_num,
+            "next_page": bookmarks.next_num,
+            "has_next": bookmarks.has_next,
+            "has_prev": bookmarks.has_prev,            
+            
+        }
         
-        return jsonify({'data': data}), HTTP_200_OK
+        return jsonify({'data': data, "meta": meta}), HTTP_200_OK
